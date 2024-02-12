644bdc5006fd761755e89a2f77293d254564cf2b
AMP2611 <140591937+AMP2611@users.noreply.github.com>
2024-02-12 10:55:03 +0530

    Add files via upload

---
+++ factorial_iterative.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cout<<"22CE080_ANJALI PARMAR"<<endl;
+cout<<"enter the number: "<<endl;
+cin>>n;
+int fact=1;
+int c=0;
+
+c=c+3;
+for(int i=n;i>0;i--)
+{
+fact=fact*i;
+c=c+3;
+}
+cout<<"factorial :"<<fact<<endl;
+cout<<"counter :"<<c;
+}
